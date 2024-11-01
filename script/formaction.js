c38e43ff914910eaf81a16a64d0bccdcac8ee42a
Ruan Marra <ruanm72@gmail.com>
2024-10-31 22:48:05 -0300

    adicionamos as linhas de comentario para o video

--- script/formaction.js
+++ script/formaction.js
@@ -1,5 +1,7 @@
+// para pegar parametros da url
 const urlParametros= new URLSearchParams(window.location.search)
+// selecionando elementos na pagina e injetando os parametros da url que vieram da form
 document.getElementById("name").textContent=urlParametros.get("name")
 document.getElementById("email").textContent=urlParametros.get("email")
 document.getElementById("seucarro").textContent=urlParametros.get("seucarro")
-console.log(urlParametros.get("name"))
+//console.log(urlParametros.get("name"))
--- script/index.js
+++ script/index.js
@@ -1,2 +1,3 @@
+// codigo javascript para adicionar a navbar dinamicamente em todas as paginas.
 const navbar= '<nav><ul class="navegacao metal"><li><a href="./">Home</a></li><li><a href="./form.html">Entre no Clube!</a></li><li><a href="./about.html">Sobre o Clube</a></li></ul></nav>'
 document.body.insertAdjacentHTML("afterbegin", navbar)
